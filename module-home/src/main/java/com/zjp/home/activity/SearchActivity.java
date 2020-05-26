756fe0b28bf18d67df0624f55d354fe2edc0f9d3
jsonjia <jiazjp@163.com>
2020-05-26 20:48:44 +0800

    1.增加mmkv

---
+++ module-home/src/main/java/com/zjp/home/activity/SearchActivity.java
@@ -0,0 +1,14 @@
+package com.zjp.home.activity;
+
+import com.zjp.base.activity.BaseActivity;
+
+/**
+ * Created by zjp on 2020/5/25 21:32.
+ */
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
