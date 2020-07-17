e986ceeece252ea56c61cc34b4c53dd409aa5168
jsonjia <jiazjp@163.com>
2020-07-17 22:39:03 +0800

    1.新增积分排行榜UI；

---
+++ module-mine/src/main/java/com/zjp/mine/activity/UserCenterActivity.java
@@ -0,0 +1,14 @@
+package com.zjp.mine.activity;
+
+import com.zjp.base.activity.BaseActivity;
+
+/**
+ * Created by zjp on 2020/7/17 21:54.
+ */
+public class UserCenterActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
