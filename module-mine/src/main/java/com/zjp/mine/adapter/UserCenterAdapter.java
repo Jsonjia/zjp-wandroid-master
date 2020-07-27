4283871c544ed257d0319af46e6b9682e658d4c3
jsonjia <jiazjp@163.com>
2020-07-27 22:16:33 +0800

    1.新增用户中心逻辑

--- module-mine/src/main/java/com/zjp/mine/adapter/UserCenterAdapter.java
+++ module-mine/src/main/java/com/zjp/mine/adapter/UserCenterAdapter.java
@@ -1,10 +1,7 @@
 package com.zjp.mine.adapter;
 
-import com.chad.library.adapter.base.BaseBinderAdapter;
-import com.zjp.mine.bean.UserCenter;
-
 /**
  * Created by zjp on 2020/7/26 20:55.
  */
-public class UserCenterAdapter {
+public class UserCenterAdapter  {
 }
